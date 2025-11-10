542dc5f1875a7898a38fe03ca2d7d295078f2382
Jamie Bryson <jamieab98@gmail.com>
2025-11-10 18:49:00 -0500

    Done

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,4 +1,18 @@
 import { useEffect, useState } from "react";
 
 export function useLocalStorage(key, initialValue = null) {
+    const [inputData, setInputData] = useState(() => {
+        const storedValue = localStorage.getItem(key);
+        try {
+        return storedValue !== null ? JSON.parse(storedValue) : initialValue;
+        } catch {
+            return storedValue;
+        }
+    })
+
+    useEffect(() => {
+        localStorage.setItem(key, inputData);
+    }, [key, inputData])
+
+    return [inputData, setInputData];
 }
